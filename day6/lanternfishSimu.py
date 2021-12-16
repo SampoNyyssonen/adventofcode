4bd792fcd83c35617536395a940617f96fce7f05
SampoNyy <57799346+SampoNyy@users.noreply.github.com>
2021-12-16 15:23:37 +0200

    Day6 complete

--- day6/lanternfishSimu.py
+++ day6/lanternfishSimu.py
@@ -1,3 +1,4 @@
+
 f = open("day6/6.txt", "r")
 def findNextNumbers(line, separator):
     spacepos = line.find(separator)
@@ -21,18 +22,111 @@ def fishCheck(fish):
         return 6
     else:
         return fish
-fishArr = findNextNumbers(f.readline(), ",")
+
 dayCount = 0
-print(len(fishArr))
-while dayCount < 50:
+fishNumArray = [0,0,0,0,0,0,0,0,0,0]
+fishArr = []
+
+i = 0
+
+#Selvitä montako kalaa syntyy numerosta 8,7,6,5,4,3,2,1 100 päivässä
+#Käy taulukko läpi 155 päivän kohdalla 
+#ja kerro taulukon jokainen lukema sen numeron 100 päivän lukemalla  
+
+#Selvitä 64 päivän lukemat kullekkin mahdolliselle kalalle
+while i < 9:
+    fishArr = [i]
+    dayCount = 0
+    while dayCount < 64:
+        unbornBabyCount = 0
+        for j, fish in enumerate(fishArr):
+            fishArr[j] -= 1
+            if fishArr[j] < 0:
+                unbornBabyCount +=1
+                fishArr[j] = 6
+   
+        fishArr+=[ 8 for i in range(unbornBabyCount) ]
+
+        dayCount += 1
+    fishNumArray[i] = fishArr
+    i+=1
+    print(len(fishArr))
+fish100Array = [0,0,0,0,0,0,0,0,0,0]
+i = 0
+#selvitä lukema 128 päivälle
+while i < 9:
+    fishArr = [i]
+    dayCount = 0
+    while dayCount < 2:
+        unbornBabyCount = 0
+        newFishArr = []
+        oldFishArr = []
+        for j, fish in enumerate(fishArr):
+            newFishArr += fishNumArray[fishArr[j]]
+        fishArr = newFishArr
+        newFishArr = []
+        #unbornBabyCount -= 1
+        dayCount += 1
+    fish100Array[i] = len(fishArr)
+    i+=1
+    print(len(fishArr))
+fishArr = [0]
+dayCount = 0
+fishArr = findNextNumbers(f.readline(), ",")
+fishArrBak = fishArr
+while dayCount < 64:
     unbornBabyCount = 0
-    i = 0
-    for i, fish in enumerate(fishArr):
-        fishArr[i] = fishCheck(fishArr[i])
-        if fishArr[i] == 6:
+    
+    for j, fish in enumerate(fishArr):
+        fishArr[j] -= 1
+        if fishArr[j] < 0:
             unbornBabyCount +=1
+            fishArr[j] = 6
     #while unbornBabyCount >= 1:
     fishArr+=[ 8 for i in range(unbornBabyCount) ]
     #    unbornBabyCount -= 1
     dayCount += 1
+print("testi")
 print(len(fishArr))
+
+
+allFishArrays = []
+i = 0
+#fishArr = findNextNumbers(f.readline(), ",")
+dayCount = 0
+newFishArr = []
+oldFishArr = []
+#mennään 128 päivää aloituspoolille
+fishArr = fishArrBak
+while dayCount < 1:
+    unbornBabyCount = 0
+    newFishArr = []
+    oldFishArr = []
+    for i, fish in enumerate(fishArr):
+        newFishArr += fishNumArray[fishArr[i]]
+    fishArr = newFishArr
+    newFishArr = []
+    #unbornBabyCount -= 1
+    dayCount += 1
+    allFishArrays+=fishArr
+print(len(allFishArrays))
+
+""" while dayCount < 250:
+    unbornBabyCount = 0
+    for j, fish in enumerate(fishArr):
+        fishArr[j] -= 1
+        if fishArr[j] < 0:
+            unbornBabyCount +=1
+            fishArr[j] = 6
+    #while unbornBabyCount >= 1:
+    fishArr+=[ 8 for i in range(unbornBabyCount) ]
+    #    unbornBabyCount -= 1
+    dayCount += 1
+    allFishArrays+=fishArr """
+
+#fishArr = findNextNumbers(f.readline(), ",")
+#summataan 128 päivää lisää jokaiselle poolin kalalle
+fishSum = 0
+for i, fish in enumerate(allFishArrays):
+    fishSum += fish100Array[allFishArrays[i]]
+print(fishSum)
